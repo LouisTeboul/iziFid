6a03f0f42d9e1aa12ac50d5ee6bb250717ad6a27
Louis Teboul <louis.teboul@izi-pass.com>
2015-05-13 11:51:43 +0200

    Something is blocking the plugins from phonegap build

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,48 +1,48 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
-        "id": "com.ionic.keyboard.keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
-        "id": "org.apache.cordova.console.console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/logger.js",
-        "id": "org.apache.cordova.console.logger",
-        "clobbers": [
-            "cordova.logger"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
-        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
-        "clobbers": [
-            "cordova.plugins.barcodeScanner"
-        ]
-    }
-];
-module.exports.metadata = 
+    module.exports = [
+        {
+            "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+            "id": "com.ionic.keyboard.keyboard",
+            "clobbers": [
+                "cordova.plugins.Keyboard"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+            "id": "org.apache.cordova.console.console",
+            "clobbers": [
+                "console"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.console/www/logger.js",
+            "id": "org.apache.cordova.console.logger",
+            "clobbers": [
+                "cordova.logger"
+            ]
+        },
+        {
+            "file": "plugins/org.apache.cordova.device/www/device.js",
+            "id": "org.apache.cordova.device.device",
+            "clobbers": [
+                "device"
+            ]
+        },
+        {
+            "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+            "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+            "clobbers": [
+                "cordova.plugins.barcodeScanner"
+            ]
+        }
+    ];
+    module.exports.metadata =
 // TOP OF METADATA
-{
-    "com.ionic.keyboard": "1.0.4",
-    "org.apache.cordova.console": "0.2.13",
-    "org.apache.cordova.device": "0.3.0",
-    "com.phonegap.plugins.barcodescanner": "2.2.0"
-};
+    {
+        "com.ionic.keyboard": "1.0.4",
+        "org.apache.cordova.console": "0.2.13",
+        "org.apache.cordova.device": "0.3.0",
+        "com.phonegap.plugins.barcodescanner": "2.2.0"
+    };
 // BOTTOM OF METADATA
 });
