13a54e898dabb991fecf346b94b50ef2c6a572f2
Louis Teboul <louis.teboul@izi-pass.com>
2015-08-17 10:41:54 +0200

    Initial Vidici commit
    This is a combination of 8 commits.
    - Initial commit
    
    - add miga icon
    
    - change package name to com.izipass.miga
    
    - Miga splashscreen
    
    - update ios platform
    
    - fix "+ MONTANT D' ACHAT" button
    
    - try to fix scroll position when coming back
    
    - Initial IbisKitchen commit
    
    This is a combination of 3 commits.
    - hide 'ajouter un passage' & 'montant d'achat' buttons
    
    - Initial Poulet Cent Facons commit
    
    - Initial IbisKitchen commit

--- platforms/ios/www/js/vendor/directives/account.js
+++ platforms/ios/www/js/vendor/directives/account.js
@@ -171,10 +171,11 @@ angular.module('APIServiceApp')
                             $scope.isReady = true;
                             if (data === false) {
                                 $scope.reset();
-                                navigator.notification.alert('Carte inconnue !', null, "FF Pizza", "OK");
+                                navigator.notification.alert('Carte inconnue !', null, "Miga", "OK");
 //                                $window.alert('Carte inconnue !');
                                 !$scope.isBrowser ? $rootScope.scan() : 0;
                             } else if (!data.CustomerFirstName && !data.CustomerLastName && !data.CustomerEmail) {
+                                $scope.client.barcode = $scope.barcode;
                                 $scope.reset();
                                 $scope.goRegister();
                             } else {
@@ -196,7 +197,7 @@ angular.module('APIServiceApp')
                         if (navigator.notification) {
                             navigator.notification.confirm('Êtes-vous sûr de vouloir vous déconnecter ?', function () {
                                 $scope.reset();
-                            }, "FF Pizza");
+                            }, "Miga");
                         } else {
                             $window.confirm("Êtes-vous sûr de vouloir vous déconnecter ?") ? (function () {
                                 $scope.reset();
@@ -211,13 +212,14 @@ angular.module('APIServiceApp')
                     $scope.backToLogin = function () {
                         $scope.register = false;
                         $scope.reset();
+                        window.scrollTo(0, 0);
                         !$scope.isBrowser ? $rootScope.scan() : 0;
                     };
 
                     $scope.login = function () {
                         checkBarcode($scope.form.barcode);
                         if (navigator.notification) {
-                            $scope.barcodeValid ? displayData() : navigator.notification.alert("Ce n° de carte n'est pas valide !", null, "FF Pizza", "OK");
+                            $scope.barcodeValid ? displayData() : navigator.notification.alert("Ce n° de carte n'est pas valide !", null, "Miga", "OK");
                         } else {
                             $scope.barcodeValid ? displayData() : $window.alert("Ce n° de carte n'est pas valide !");
                         }
@@ -227,7 +229,7 @@ angular.module('APIServiceApp')
                         if ($scope.auto) {
                             checkBarcode($scope.form.barcode);
                             if (navigator.notification) {
-                                $scope.barcodeValid ? displayData() : navigator.notification.alert("Ce n° de carte n'est pas valide !", null, "FF Pizza", "OK");
+                                $scope.barcodeValid ? displayData() : navigator.notification.alert("Ce n° de carte n'est pas valide !", null, "Miga", "OK");
                             } else {
                                 $scope.barcodeValid ? displayData() : $window.alert("Ce n° de carte n'est pas valide !");
                             }
@@ -285,7 +287,7 @@ angular.module('APIServiceApp')
 
                         if (~~balance.Value < ~~val) {
                             if (navigator.notification) {
-                                $scope.barcodeValid ? displayData() : navigator.notification.alert('Ce montant est supérieur au total de la cagnotte', null, "FF Pizza", "OK");
+                                $scope.barcodeValid ? displayData() : navigator.notification.alert('Ce montant est supérieur au total de la cagnotte', null, "Miga", "OK");
                             } else {
                                 $scope.barcodeValid ? displayData() : $window.alert('Ce montant est supérieur au total de la cagnotte');
                             }
@@ -429,7 +431,7 @@ angular.module('APIServiceApp')
                         if (navigator.notification) {
                             navigator.notification.confirm('Voulez-vous utiliser cette offre ?', function () {
                                 $scope.useOffer(offer);
-                            }, "FF Pizza");
+                            }, "Miga");
                         } else {
                             var doUse = $window.confirm("Voulez-vous utiliser cette offre ?");
                             if (doUse) $scope.useOffer(offer);
@@ -465,7 +467,7 @@ angular.module('APIServiceApp')
                         if (navigator.notification) {
                             navigator.notification.confirm("Confirmez-vous que ce client est passé en caisse sans utiliser d'offre et/ou d'avoir fidélité ?", function () {
                                 $scope.addPassage();
-                            }, "FF Pizza");
+                            }, "Miga");
                         } else {
                             var doUse = $window.confirm("Confirmez-vous que ce client est passé en caisse sans utiliser d'offre et/ou d'avoir fidélité ?");
                             if (doUse) $scope.addPassage();
